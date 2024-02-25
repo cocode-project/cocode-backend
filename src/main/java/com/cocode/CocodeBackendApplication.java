29c77a8c61f0a572a04f3b76132100d3e9c185c9
Joshua Frías <metatron1495@gmail.com>
2024-02-24 18:16:51 -0600

    First init

---
+++ src/main/java/com/cocode/CocodeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cocode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocodeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocodeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cocode/CocodeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cocode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocodeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
